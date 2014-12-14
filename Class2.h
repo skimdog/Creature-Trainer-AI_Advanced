efe6caf71697d390cf7914600bc9158d4dbbc365
Sam <samuelmk@umich.edu>
2014-12-14 17:02:27 -0500

    Do not need class2

--- Class2.h
+++ Class2.h
@@ -12,32 +12,11 @@
 #ifndef __CreatureTrainer__Class2__
 #define __CreatureTrainer__Class2__
 
-#include <iostream>
-using namespace std;
-
 // vvvvvv Your implementations go between this line…
 
-class ParseHelp
-{
-public:
-    
-    /**
-     * Requires: Nothing.
-     * Modifies: Nothing.
-     * Effects:  Default constructor
-     */
-    ParseHelp();
-    
-    /**
-     * Requires: Nothing.
-     * Modifies: Nothing.
-     * Effects:  Parses out name and level from name-level
-                 Changes reference parameters name & level
-                 Ex: before: name-level = "Dugong-0"
-                     after: name = "Dugong" & level = 0
-     */
-    void parseNameAndLevel(string& name, int& level, string name_level);
-};
+
+
+
 
 // ^^^^^^ And this line. That's it!
 
